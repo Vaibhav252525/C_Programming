cf8fb51395ea5dd1e614ece00bdd647c6134f898
Vaibhav252525 <keniv935@gmail.com>
2024-03-14 12:33:31 +0530

    initial commit

---
+++ Loops/char.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    for ( char ch = 'a'; ch <='z'; ch++)
+    {
+        printf("%c\n",ch);
+    }
+    
+}
---
+++ Loops/float1-5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    for ( float i = 1.0; i <=10.0; i++)
+    {
+        printf("%f\n",i);
+    }
+    
+}
